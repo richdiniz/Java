f7bc257fd09e46a840a26ff4e00dbb8a64e4c903
Henrique Diniz <luisdiniz2104@gmail.com>
2026-09-01 17:13:04 -0300

    .

--- 2026-estudosIndependentes/arraybidimensionais/ativ3.java
+++ 2026-estudosIndependentes/arraybidimensionais/ativ3.java
@@ -7,12 +7,15 @@ public class ativ3 {
         System.out.println("----------------------");
 
         //int[][] matriz=new int[2][3];
-        int[][] matriz = [[1,2,3],[55,67,42]];
+        int[][] matriz = {
+            {1,2,3},
+            {55,67,42}
+        };
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print(matriz[i][j]+"\t");
             }
             System.out.println();
-        }     
+        }
     }
 }
