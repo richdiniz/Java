2d1e70cbc996701a5a70ae73e1a63dcaf475689d
Henrique Diniz <luisdiniz2104@gmail.com>
2026-09-01 22:11:34 -0300

    .

---
+++ 2026-estudosIndependentes/estruturasheterogeneas/Heroi.java
@@ -0,0 +1,8 @@
+package estruturasheterogeneas;
+public class Heroi {
+    String nome;
+    String poder;
+    int nivel;
+    double dano;
+    boolean id_screta;
+}
---
+++ 2026-estudosIndependentes/estruturasheterogeneas/ativ1.java
@@ -0,0 +1,10 @@
+package estruturasheterogeneas;
+public class ativ1 {
+    public static void main(String[] args) {
+        System.out.println("###ESTRUTURAS HETEROGÊNEAS###");
+        System.out.println("--HEROI--");
+        Heroi parker=new Heroi();
+        parker.nome="Homem aranha";
+        System.out.println(parker.nome);
+    }
+}
